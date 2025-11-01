530ee636ab46781f4b426bac5ec3a32cb01d3dec
L1anr1 <124812538+L1anr1@users.noreply.github.com>
2025-11-01 20:19:18 +0200

    Update sum_of_cubes_even.py

--- sum_of_cubes_even.py
+++ sum_of_cubes_even.py
@@ -1,4 +1,12 @@
 def sum_of_cubes_even(n):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if type(n) is int and n>=0:
+        if n>2000:
+            print("WARNING!!!")
+        sum = 0
+        for i in range(n+1):
+            if i%2==0:
+                sum+=i**3
+        f = float(sum)
+        return f
+else:
+    return -1
