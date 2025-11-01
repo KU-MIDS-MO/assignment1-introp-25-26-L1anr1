bdd2ac2539506fb6a25a0d3ad0d7b6c167a29538
L1anr1 <124812538+L1anr1@users.noreply.github.com>
2025-11-01 20:05:05 +0200

    Update is_strictly_increasing_digits.py

--- is_strictly_increasing_digits.py
+++ is_strictly_increasing_digits.py
@@ -1,4 +1,9 @@
 def is_strictly_increasing_digits(n):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if type(n) is int and n>=0:
+        s = str(n)
+        for i in range(len(s)-1):
+            if s[i]>=s[i+1]:
+                return False
+        return True
+    else:
+        return -1
