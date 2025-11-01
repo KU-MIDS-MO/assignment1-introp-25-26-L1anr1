2d60bbcbd8c27b6b7a9a03141e12d008c85f22cf
L1anr1 <124812538+L1anr1@users.noreply.github.com>
2025-11-01 20:38:00 +0200

    Update count_digits_greater_than.py

--- count_digits_greater_than.py
+++ count_digits_greater_than.py
@@ -1,4 +1,10 @@
 def count_digits_greater_than(n, t):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if (type(n) is int and n>=0) and (type(t) is int and t>=0 and t<=9):
+        s = str(n)
+        count = 0
+        for i in range(len(s)):
+            if int(s[i])>t:
+                count+=1
+        return count
+    else:
+        return -1
